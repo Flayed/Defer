7c1d91311ebb41e49f649b5966107ce7cdbb5746
Robert P. Darchuk <novaprogramming@gmail.com>
2017-09-14 21:27:21 -0500

    Adding remaining functionality

--- Flayed.Defer.UnitTests/DefermentTests.cs
+++ Flayed.Defer.UnitTests/DefermentTests.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,6 +22,66 @@ namespace Flayed.Deferment.UnitTests
         }
 
         [Test]
+        public void Defer_NoAction_DoesNotAddToTaskList()
+        {
+            Deferment deferment = new Deferment();
+
+            deferment.Defer((Action)null, 5000, CancellationToken.None);
+
+            deferment.Tasks.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Defer_NoFunc_DoesNotAddToTaskList()
+        {
+            Deferment deferment = new Deferment();
+
+            deferment.Defer((Func<Task>)null, 5000, CancellationToken.None);
+
+            deferment.Tasks.Should().BeEmpty();
+        }
+
+        [Test]
+        public async Task Defer_RunsAction()
+        {
+            bool invokedAction = false;
+            Deferment deferment = new Deferment();
+
+            deferment.Defer(() => { invokedAction = true; }, 0, CancellationToken.None);
+
+            await Task.WhenAll(deferment.Tasks.ToArray());
+
+            invokedAction.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Defer_RunsFunc()
+        {
+            bool invokedFunc = false;
+            Deferment deferment = new Deferment();
+
+            deferment.Defer(async () => { await Task.Delay(0); invokedFunc = true; }, 0);
+
+            await Task.WhenAll(deferment.Tasks.ToArray());
+
+            invokedFunc.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Defers_WaitsBeforeInvokingAction()
+        {
+            int delay = 10;
+            Stopwatch sw = Stopwatch.StartNew();
+            Deferment deferment = new Deferment();
+
+            deferment.Defer(() => { sw.Stop(); }, delay, CancellationToken.None);
+
+            await Task.WhenAll(deferment.Tasks.ToArray());
+
+            sw.ElapsedMilliseconds.Should().BeGreaterOrEqualTo(delay);
+        }
+
+        [Test]
         public async Task Defer_RemovesFromTasklistWhenComplete()
         {
             Deferment deferment = new Deferment();
@@ -33,5 +95,52 @@ namespace Flayed.Deferment.UnitTests
             deferment.Tasks.Should().BeEmpty();
         }
 
+        [Test]
+        public void Defer_Cancellation_DoesNotThrow()
+        {
+            Deferment deferment = new Deferment();
+
+            CancellationTokenSource cts = new CancellationTokenSource(1);
+
+            try
+            {
+                deferment.Defer(() => { }, 10, cts.Token);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"{ex.GetType()} should not have been thrown");
+            }
+        }
+
+        [Test]
+        public async Task Defer_CancellationTokenIsCancelled_DoesNotInvokeAction()
+        {
+            bool invokedAction = false;
+            Deferment deferment = new Deferment();
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            cts.Cancel();
+
+            deferment.Defer(() => { invokedAction = true; }, 0, cts.Token);
+
+            await Task.WhenAll(deferment.Tasks.ToArray());
+
+            invokedAction.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task Defer_CancellationDuringDelay_DoesNotInvokeAction()
+        {
+            bool invokedAction = false;
+            Deferment deferment = new Deferment();
+
+            CancellationTokenSource cts = new CancellationTokenSource(5);
+
+            deferment.Defer(() => { invokedAction = true; }, 10, cts.Token);
+
+            await Task.WhenAll(deferment.Tasks.ToArray());
+
+            invokedAction.Should().BeFalse();
+        }
     }
 }
--- Flayed.Deferment/Deferment.cs
+++ Flayed.Deferment/Deferment.cs
@@ -17,9 +17,73 @@ namespace Flayed.Deferment
         /// </summary>
         public IEnumerable<Task> Tasks => _tasks.Values;
 
-        public void Defer(Action action, int delay, CancellationToken cancellationToken)
+        /// <summary>
+        /// Defers the provided action until the supplied delay has elapsed, monitoring the cancellation token.
+        /// </summary>
+        /// <param name="action">The action to invoke after the supplied delay has elapsed.</param>
+        /// <param name="delay">The number of miliseconds to delay before invoking the action.</param>
+        /// <param name="cancellationToken">The cancellation token to observe.</param>
+        public void Defer(Action action, int delay, CancellationToken cancellationToken) => Defer(action, null, delay, cancellationToken);
+
+        /// <summary>
+        /// Defers the provided action until the supplied delay has elapsed.
+        /// </summary>
+        /// <param name="action">The action to invoke after the supplied delay has elapsed.</param>
+        /// <param name="delay">The number of miliseconds to delay before invoking the action.</param>
+        public void Defer(Action action, int delay) => Defer(action, null, delay, CancellationToken.None);
+
+        /// <summary>
+        /// Defers the provided async func until the supplied delay has elapsed, monitoring the cancellation token.
+        /// </summary>
+        /// <param name="func">The async func to invoke after the delay has elapsed.</param>
+        /// <param name="delay">The number of miliseconds to delay before invoking the async func.</param>
+        /// <param name="cancellationToken">The cancellation token to observe.</param>
+        public void Defer(Func<Task> func, int delay, CancellationToken cancellationToken) => Defer(null, func, delay, cancellationToken);
+
+        /// <summary>
+        /// Defers the provided async func until the supplied delay has elapsed.
+        /// </summary>
+        /// <param name="func">The async func to invoke after the delay has elapsed.</param>
+        /// <param name="delay">The number of miliseconds to delay before invoking the async func.</param>
+        public void Defer(Func<Task> func, int delay) => Defer(null, func, delay, CancellationToken.None);
+
+        /// <summary>
+        /// Runs an action on a separate thread.
+        /// </summary>
+        /// <param name="action">The action to invoke.</param>
+        public void Run(Action action) => Defer(action, null, 0, CancellationToken.None);
+
+        /// <summary>
+        /// Runs an action on a separate thread while observing the provided cancellation token.
+        /// </summary>
+        /// <param name="action">The action to invoke.</param>
+        /// <param name="cancellationToken">The cancellation token to observe.</param>
+        public void Run(Action action, CancellationToken cancellationToken) => Defer(action, null, 0, cancellationToken);
+
+        /// <summary>
+        /// Runs an async func on a separate thread.
+        /// </summary>
+        /// <param name="func">The async func to invoke.</param>
+        public void Run(Func<Task> func) => Defer(null, func, 0, CancellationToken.None);
+
+        /// <summary>
+        /// Runs an async func on a separate thread while observing the provided cancellation token.
+        /// </summary>
+        /// <param name="func">The async func to invoke.</param>
+        /// <param name="cancellationToken">The cancellation token to observe.</param>
+        public void Run(Func<Task> func, CancellationToken cancellationToken) => Defer(null, func, 0, cancellationToken);
+
+        /// <summary>
+        /// Defers the provided action until the supplied delay has elapsed, monitoring the cancellation token.
+        /// </summary>
+        /// <param name="action">The action to invoke after the supplied delay has elapsed.</param>
+        /// <param name="func">The async func to invoke after the supplied delay has elapsed.</param>
+        /// <param name="delay">The number of miliseconds to delay before invoking the action.</param>
+        /// <param name="cancellationToken">The cancellation token to observe.</param>
+        private void Defer(Action action, Func<Task> func, int delay, CancellationToken cancellationToken)
         {
-            if (action == null) return;
+            if (func == null && action == null) return;
+            if (cancellationToken.IsCancellationRequested) return;
             Guid key = Guid.NewGuid();
             _tasks.TryAdd(key, Task.Run(async () =>
             {
@@ -27,10 +91,14 @@ namespace Flayed.Deferment
                 {
                     if (delay > 0)
                     {
-                        await Task.Delay(delay, cancellationToken);
+                        await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
                     }
 
-                    action.Invoke();
+                    if (cancellationToken.IsCancellationRequested)
+                        return;
+
+                    if (action != null) action.Invoke();
+                    if (func != null) await func.Invoke().ConfigureAwait(false);
                 }
                 catch (OperationCanceledException)
                 {
@@ -47,6 +115,10 @@ namespace Flayed.Deferment
             }, cancellationToken));
         }
 
+        /// <summary>
+        /// Removes the task with the provided key from the task list
+        /// </summary>
+        /// <param name="key">The key of the task to remove from the task list</param>
         private void RemoveTask(Guid key)
         {
             Task t;
